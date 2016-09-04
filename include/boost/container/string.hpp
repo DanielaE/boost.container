66953e17f46cbf25c9c3c736b51ba453fbe665e7
Daniela Engert <dani@ngrt.de>
2016-09-04 18:18:48 +0200

    replace deprecated binders and adapters by more modern equivalents
    Signed-off-by: Daniela Engert <dani@ngrt.de>
    (cherry picked from commit c794ebc37dc365d63af68dd5dbbfe4ded226a7a4)

--- include/boost/container/string.hpp
+++ include/boost/container/string.hpp
@@ -542,7 +542,7 @@ class basic_string
       bool operator()(const typename Tr::char_type& x) const
       {
          return std::find_if(m_first, m_last,
-                        std::bind1st(Eq_traits<Tr>(), x)) == m_last;
+                        [&](argument_type ch) { return Eq_traits<Traits>()(ch, x); }) == m_last;
       }
    };
    #endif   //#ifndef BOOST_CONTAINER_DOXYGEN_INVOKED
@@ -2300,7 +2300,7 @@ class basic_string
          pointer finish = addr + sz;
          const const_iterator result =
             std::find_if(addr + pos, finish,
-                  std::bind2nd(Eq_traits<Traits>(), c));
+                   [&](value_type ch) { return Eq_traits<Traits>()(ch, c); });
          return result != finish ? result - begin() : npos;
       }
    }
@@ -2372,7 +2372,7 @@ class basic_string
          const const_iterator last = begin() + container_detail::min_value(len - 1, pos) + 1;
          const_reverse_iterator rresult =
             std::find_if(const_reverse_iterator(last), rend(),
-                  std::bind2nd(Eq_traits<Traits>(), c));
+                   [&](value_type ch) { return Eq_traits<Traits>()(ch, c); });
          return rresult != rend() ? (rresult.base() - 1) - begin() : npos;
       }
    }
@@ -2550,7 +2550,7 @@ class basic_string
          const pointer finish = addr + this->priv_size();
          const const_iterator result
             = std::find_if(addr + pos, finish,
-                     std::not1(std::bind2nd(Eq_traits<Traits>(), c)));
+                  [&](value_type ch) { return !Eq_traits<Traits>()(ch, c); });
          return result != finish ? result - begin() : npos;
       }
    }
@@ -2617,7 +2617,7 @@ class basic_string
          const const_iterator last = begin() + container_detail::min_value(len - 1, pos) + 1;
          const const_reverse_iterator rresult =
             std::find_if(const_reverse_iterator(last), rend(),
-                  std::not1(std::bind2nd(Eq_traits<Traits>(), c)));
+                  [&](value_type ch) { return !Eq_traits<Traits>()(ch, c); });
          return rresult != rend() ? (rresult.base() - 1) - begin() : npos;
       }
    }
